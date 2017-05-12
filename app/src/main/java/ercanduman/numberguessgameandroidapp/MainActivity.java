bfb5491df43272edca43e20276562c21d4eb503f
Ercan Duman <ercanduman30@gmail.com>
2017-05-12 14:17:12 +0300

    Views added, Button clicks handled, Check number and start game method added

--- app/src/main/java/ercanduman/numberguessgameandroidapp/MainActivity.java
+++ app/src/main/java/ercanduman/numberguessgameandroidapp/MainActivity.java
@@ -5,15 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button checkButton, replayButton;
     private TextView remainTrailsTextView, resultTextView;
-    private EditText userGuessText;
+    private EditText userGuessEditText;
+
+    private static int TRIALS = 10;
+    private static int GUESSED_NUMBER;
+    private int usersNumberGuess;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +45,78 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeViews() {
+        //whenever app created, a new number will be picked randomly
+        GUESSED_NUMBER = new Random().nextInt(100);
+
         //Buttons
         checkButton = (Button) findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(this);
+
         replayButton = (Button) findViewById(R.id.buttonReplay);
+        replayButton.setOnClickListener(this);
+
 
         //TextViews
+        resultTextView = (TextView) findViewById(R.id.tvResult);
+        remainTrailsTextView = (TextView) findViewById(R.id.tvRemainTrials);
+
 
         //User input EditTexts
+        userGuessEditText = (EditText) findViewById(R.id.userGuessText);
 
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.checkButton:
+                checkNumberAndStartGame();
+                break;
+            case R.id.buttonReplay:
+                startNewGame();
+                break;
+        }
+    }
+
+    private void checkNumberAndStartGame() {
+        Toast.makeText(this, "Check Button clicked!", Toast.LENGTH_SHORT).show();
+        String userCurrentInput = userGuessEditText.getText().toString().trim();
+
+        if (userCurrentInput.length() == 0) {
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        usersNumberGuess = Integer.parseInt(userCurrentInput);
+
+        if (usersNumberGuess > GUESSED_NUMBER) {
+            resultTextView.setText("It is smaller than your guess");
+            TRIALS--;
+            remainTrailsTextView.setText("You have " + TRIALS + " trials left");
+        } else if (usersNumberGuess < GUESSED_NUMBER) {
+            resultTextView.setText("It is bigger than your guess");
+            TRIALS--;
+            remainTrailsTextView.setText("You have " + TRIALS + " trials left");
+        } else {
+            resultTextView.setText("You found it in " + (10 - TRIALS) + " trials!");
+            TRIALS = 10;
+            remainTrailsTextView.setText("Congratulations!");
+
+        }
+
+        if (TRIALS == 0) {
+            resultTextView.setText("Trials are finished!");
+            remainTrailsTextView.setText("GAME OVER!");
+            TRIALS = 10;
+        }
+    }
+
+    private void startNewGame() {
+        Toast.makeText(this, "Replay Button clicked!", Toast.LENGTH_SHORT).show();
+    }
+
+
+    // HANDLE MENU ITEMS
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
